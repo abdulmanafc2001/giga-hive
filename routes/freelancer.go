9790984489d6fb69ea04b1c05c583fbf543bba2a
Abdul Manaf C <manafabdulc@gmail.com>
2024-01-01 19:52:50 +0530

    added freelancer change the status of the accepted auction added routes

--- routes/freelancer.go
+++ routes/freelancer.go
@@ -12,13 +12,14 @@ func FreelancerRoutes(router *gin.Engine) {
 		r.POST("/signup", controllers.FreelancerSignup)
 		r.POST("/signup/otpverification", controllers.ValidateOTP)
 		r.POST("/login", controllers.Login)
-		r.POST("/logout",m.FreelancerAuthentication,controllers.Logout)
+		r.POST("/logout", m.FreelancerAuthentication, controllers.Logout)
 
 		r.GET("/profile", m.FreelancerAuthentication, controllers.GetProfile)
 		r.PUT("/profile/changepassword", m.FreelancerAuthentication, controllers.ChangePassword)
 
 		r.GET("/bid/showallbid", m.FreelancerAuthentication, controllers.ShowAllBids)
 		r.POST("/bid/auction", m.FreelancerAuthentication, controllers.AuctionForBid)
+		r.PUT("/auction/changestatus", m.FreelancerAuthentication, controllers.ChangeAcceptedAuctionStatus)
 	}
 
 }
